cca085f6646bb440b8b82c3a4a88ab94f7530cdd
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-05-21 16:10:41 +0300

    Задача 4.3.7 третья проверка

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in); // саздание экземпляра сканера
+        String s = scanner.nextLine(); // чтение с клавиатуры символов в стринговую переменную s
 
-        double sum = 0.0;
+        double sum = 0.0; // инциализация дабл переменной sum
 
-        String[] strs = s.split("\\s+");
+        String[] strs = s.split("\\s+"); // разделение строки по пробелам в массив
 
-        for (String str : strs) {
+        for (String str : strs) { // цикл по каждому элементу массива
             try {
-                double number = Double.parseDouble(str);
-                sum += number;
-            } catch (NumberFormatException ignored) {
+                double number = Double.parseDouble(str); // попытка преобразования строки в дабл
+                sum += number; // прибравление к сумме
+            } catch (NumberFormatException ignored) { // игнорирование исключения
             }
         }
 
-        System.out.printf("%.6f", sum);
-        scanner.close();
+        System.out.printf("%.6f", sum); // вывод итоговой суммы
+        scanner.close(); // закрытие сканера
     }
 }
